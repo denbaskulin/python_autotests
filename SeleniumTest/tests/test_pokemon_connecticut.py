380815b26a25d80d2c45a118c0e2a3a6b391253f
denbaskulin <133255742+denbaskulin@users.noreply.github.com>
2024-04-04 20:36:29 +0300

    Update test_pokemon_connecticut.py

--- SeleniumTest/tests/test_pokemon_connecticut.py
+++ SeleniumTest/tests/test_pokemon_connecticut.py
@@ -2,13 +2,12 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.me/v2/'
-HEADERS = {'Content-Type' : 'application/json', 'trainer_token': '8575331e0b41e9a84d32f5b74962bf33'}
-TOKEN = '8575331e0b41e9a84d32f5b74962bf33'
+HEADERS = {'Content-Type' : 'application/json', 'trainer_token': 'TOKEN'}
+TOKEN = 'TOKEN'
 
 def test_status_code():
     response = requests.get(url = f'{URL}trainers', params = {"trainer_id" : 2173})
     assert response.status_code == 200
-
-    def test_part_of_response():
-        response = requests.get(url = f'{URL}trainers', params = {"trainer_id" : 2173})
-        assert response.json()['trainer_name'] == 'Dench'
+def test_part_of_response():
+    response = requests.get(url = f'{URL}trainers', params = {"trainer_id" : 2173})
+    assert response.json()['trainer_name'] == 'Dench'
